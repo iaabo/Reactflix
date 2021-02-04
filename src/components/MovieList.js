2f5cb6c167c39bdc4bd540e6fbe5a9aa1a874890
iaabo <insbarroso@gmail.com>
2021-02-04 21:26:43 +0000

    pagination for movies funcionality done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import ReactPaginate from "react-paginate";
 import "./Movie.css";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [genres, setGenres] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [moviesPerPage, setMoviePerPage] = useState(9);
 
   useEffect(() => {
     fetch(
@@ -22,13 +25,36 @@ const MovieList = () => {
       });
   }, []);
 
+
+
+  const pagesVisited = currentPage * moviesPerPage;
+
+  //change page
+
+  const pageCount = Math.ceil(movies.length / moviesPerPage);
+  const changePage = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
   return (
     <div className="container">
-       
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttns"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
       <div className="row justify-content-md-center">
-        {movies.map((movie) => (
-          <Movie {...movie} key={movie.id} />
-        ))}
+        {movies
+          .slice(pagesVisited, pagesVisited + moviesPerPage)
+          .map((movie) => (
+            <Movie {...movie} key={movie.id} />
+          ))}
       </div>
     </div>
   );
