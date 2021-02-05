9ddfcf38f3932c5ff8783842761158c337b41f41
iaabo <insbarroso@gmail.com>
2021-02-05 12:25:05 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import MovieList from "./components/MovieList";
 import NavBar from "./components/NavBar";
 import WatchList from "./components/WatchList";
+import FooterMovie from "./components/FooterMovie";
 import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
     <div>
       <GlobalProvider>
-      <NavBar />
+        <NavBar />
         <Switch>
           <Route exact path="/" component={MovieList} />
           <Route path="/watchlist" component={WatchList} />
           <Redirect to="/" />
         </Switch>
       </GlobalProvider>
+      <FooterMovie />
     </div>
   );
 }
---
+++ src/components/FooterMovie.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const FooterMovie = () => {
+  return (
+    <div>
+      <div className="text-center p-3 text-light">
+        
+          <a
+            className="text-light"
+            href="https://www.linkedin.com/in/inesbarroso/"
+          >
+            © Reactflix - Done by Inês Barroso
+          </a>
+        
+      </div>
+    </div>
+  );
+};
+
+export default FooterMovie;
